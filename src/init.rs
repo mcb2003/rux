01c5f2fd12ac02264c868d8e1240749e34056e08
mikeybuchan@hotmail.co.uk <mikeybuchan@hotmail.co.uk>
2021-06-30 16:56:59 +0100

    Separate memory-area printing and ELF section printing into their own functions.

--- src/init.rs
+++ src/init.rs
@@ -13,20 +13,28 @@ pub extern "C" fn kernel_main(multiboot_info: usize) -> ! {
     let cmdline = multiboot_info.command_line_tag().map(|t| t.command_line()).unwrap_or("");
     println!("Kernel cmdline: {:?}", cmdline);
 
-    let mm_tag = multiboot_info.memory_map_tag()
+    print_memory_areas(&multiboot_info);
+    print_elf_sections(&multiboot_info);
+
+    crate::hlt_loop();
+}
+
+fn print_memory_areas(mb: &multiboot2::BootInformation) {
+    let mm_tag = mb.memory_map_tag()
     .expect("Multiboot2 structure must have a memory map tag");
 println!("memory areas:");
 println!("{:17} {:10} {:10} {}", "Type", "Start", "End", "Length");
 for area in mm_tag.memory_areas() {
     println!("{:17?} {:#010x} {:#010x} {}", area.typ(), area.start_address(), area.end_address(), area.size());
 }
+}
 
-let elf_tag = multiboot_info.elf_sections_tag()
+fn print_elf_sections(mb: &multiboot2::BootInformation) {
+let elf_tag = mb.elf_sections_tag()
     .expect("Multiboot2 structure must have an ELF sections tag");
     println!("ELF Sections:");
     println!("{:24} {:10} {:10} {}", "Type", "Start", "End", "Name");
     for section in elf_tag.sections() {
         println!("{:24?} {:#010x} {:#010x} {}", section.section_type(), section.start_address(), section.end_address(), section.name());
     }
-    crate::hlt_loop();
 }
