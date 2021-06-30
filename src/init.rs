ad3479ae04216dfe0d64b1bfd4c14447927e0de9
mikeybuchan@hotmail.co.uk <mikeybuchan@hotmail.co.uk>
2021-06-30 15:27:29 +0100

    Print kernel command-line if any.

--- src/init.rs
+++ src/init.rs
@@ -9,6 +9,8 @@ pub extern "C" fn kernel_main(multiboot_info: usize) -> ! {
     crate::interrupts::init();
     let multiboot_info = unsafe { multiboot2::load(multiboot_info) };
     let bootloader = multiboot_info.boot_loader_name_tag().map(|t| t.name()).unwrap_or("unknown bootloader");
-    println!("Starting {} kernel v.{}, booted by {}", KERNEL_NAME, KERNEL_VERSION, bootloader);
+    println!("Starting {} kernel v.{}, booted by {:?}", KERNEL_NAME, KERNEL_VERSION, bootloader);
+    let cmdline = multiboot_info.command_line_tag().map(|t| t.command_line()).unwrap_or("");
+    println!("Kernel cmdline: {:?}", cmdline);
     crate::hlt_loop();
 }
