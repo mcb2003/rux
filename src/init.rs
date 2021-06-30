dfbef11f15f78aedb1bbd40ddd9008e0bf348045
mikeybuchan@hotmail.co.uk <mikeybuchan@hotmail.co.uk>
2021-06-30 16:17:06 +0100

    Merge ELF sections with ld and print them in kernel_main.

--- src/init.rs
+++ src/init.rs
@@ -20,5 +20,13 @@ println!("{:17} {:10} {:10} {}", "Type", "Start", "End", "Length");
 for area in mm_tag.memory_areas() {
     println!("{:17?} {:#010x} {:#010x} {}", area.typ(), area.start_address(), area.end_address(), area.size());
 }
+
+let elf_tag = multiboot_info.elf_sections_tag()
+    .expect("Multiboot2 structure must have an ELF sections tag");
+    println!("ELF Sections:");
+    println!("{:24} {:10} {:10} {}", "Type", "Start", "End", "Name");
+    for section in elf_tag.sections() {
+        println!("{:24?} {:#010x} {:#010x} {}", section.section_type(), section.start_address(), section.end_address(), section.name());
+    }
     crate::hlt_loop();
 }
