77936c79b2a771324a5a2a8027ec8bd24b68d5c3
mcb2003 <mikeybuchan@hotmail.co.uk>
2021-10-09 19:11:52 +0100

    Much friendlier error messages from assembly!
    The error sub-routine now takes a pointer to a null-terminated string,
    and prints it, proceeded by "ERROR: " instead of "ERR: ".

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -3,10 +3,11 @@ extern long_mode_start
 
 section .text
 bits 32
+
 start:
     ; Set the stack pointer
     mov esp, stack_top
-    ; Move Multiboot info struct pointer to edit
+    ; Move Multiboot info struct pointer to edi
     mov edi, ebx
 
     ; Sanity checks
@@ -24,16 +25,27 @@ start:
     jmp gdt64.code:long_mode_start
 
 ; Displays an error code on the screen and halts indefinitely
-; Error code (ASCII char) goes in al
-; Todo: Expand this to copy a string into screen RAM, for nicer error messages
+; edx should contain a pointer to a null-terminated error message
 error:
+; Print "ERROR: "
     mov dword [0xb8000], 0x04520445
-    mov dword [0xb8004], 0x043a0452
-    mov dword [0xb8008], 0x04200420
-    mov byte  [0xb800a], al
+    mov dword [0xb8004], 0x044f0452
+    mov dword [0xb8008], 0x043a0452
+    mov word [0xb800c], 0x0420
+    mov ecx, 0xb800e
+    ; Print the null-terminated string
+    .output:
+    cmp byte [edx], 0
+    je .halt
+    ; Output one character
+    mov al, [edx]
+    mov [ecx], al
+    add ecx, 2 ; Skip the color attributes
+    inc edx
+    jmp .output
     cli
-    loop: hlt
-    jmp loop 
+    .halt: hlt
+    jmp .halt
 
 ; Checks if we were started from a Multiboot2 bootloader
 check_multiboot:
@@ -41,7 +53,7 @@ check_multiboot:
     jne .no_multiboot
     ret
     .no_multiboot:
-        mov al, "m"
+        mov edx, no_multiboot_msg
         jmp error
 
 ; Check if CPUID is supported by attempting to flip the ID bit (bit 21)
@@ -50,37 +62,33 @@ check_cpuid:
     pushfd
     pop eax
     mov ecx, eax
-
     ; Flip the ID bit
     xor eax, 1 << 21
-
+; Try to reload modified flags
     push eax
     popfd
-
-    ; Copy FLAGS back to EAX (with the flipped bit if CPUID is supported)
+    ; Copy FLAGS back to eax (with the flipped bit if CPUID is supported)
     pushfd
     pop eax
-
     ; Restore FLAGS from the old version stored in ECX (i.e. flipping the
     ; ID bit back if it was ever flipped).
     push ecx
     popfd
-
+; Check if the bit was successfully flipped
     cmp eax, ecx
     je .no_cpuid
     ret
     .no_cpuid:
-       mov al, "c"
+       mov edx, no_cpuid_msg
         jmp error
 
-; Check whether the processor supports the 64 bit long mode
+; Check whether the CPU supports the 64 bit long mode
 check_long_mode:
 ; Get the highest supported cpuid argument
     mov eax, 0x80000000    ; implicit argument for cpuid
     cpuid
     cmp eax, 0x80000001
     jb .no_long_mode       ; the CPU is too old for long mode
-
     ; use extended info to test if long mode is available
     mov eax, 0x80000001    ; extended processor info
     cpuid                  ; returns various feature bits in ecx and edx
@@ -88,7 +96,7 @@ check_long_mode:
     jz .no_long_mode
     ret
     .no_long_mode:
-        mov al, "l"
+        mov edx, no_long_mode_msg
         jmp error
 
 ; Identity map the first 1 GiB of the kernel and setup paging
@@ -97,14 +105,12 @@ setup_paging:
     mov eax, p3_table
     or eax, 0b11 ; present | writable
     mov [p4_table], eax
-
     ; Map the first p3 entry to the p2 table
     mov eax, p2_table
     or eax, 0b11 ; present | writable
     mov [p3_table], eax
-
         ; map each P2 entry to a huge 2MiB page
-    mov ecx, 0
+    xor ecx, ecx
     .map_p2_table:
         ; map ecx-th P2 entry to a huge page that starts at address 2MiB*ecx
         mov eax, 0x200000  ; 2MiB
@@ -115,24 +121,22 @@ setup_paging:
         inc ecx
         cmp ecx, 512
         jne .map_p2_table
+    ret
 
+; Enable the newly set up page-tables
+enable_paging:
     ; Load p4 address to cr3
     mov eax, p4_table
     mov cr3, eax
-    ret
-
-enable_paging:
     ; enable PAE in cr4
     mov eax, cr4
     or eax, 1 << 5
     mov cr4, eax
-
     ; set the long mode bit in the EFER MSR
     mov ecx, 0xC0000080
     rdmsr
     or eax, 1 << 8 ; LM bit
     wrmsr
-
     ; enable paging in the cr0 register
     mov eax, cr0
     or eax, 1 << 31 ; PG bit
@@ -140,8 +144,9 @@ enable_paging:
     ret
 
 section .bss
-; Page tables
 align 4096
+
+; Page tables
 p4_table:
     resb 4096
 p3_table:
@@ -155,6 +160,11 @@ stack_bottom:
 stack_top:
 
 section .rodata
+; Error messages
+no_multiboot_msg: db "Must be booted with a multiboot bootloader", 0
+no_cpuid_msg: db "The CPU does not support CPUID", 0
+no_long_mode_msg: db "The CPU is not x86_64 compatible", 0
+
 ; The 64 bit Global Descriptor Table
 gdt64:
     dq 0 ; zero entry
