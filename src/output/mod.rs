bc906d55009d5eccd735795a1f76750841b89345
mcb2003 <mikeybuchan@hotmail.co.uk>
2021-10-09 18:28:43 +0100

    Add serial output to make debugging easier.

--- src/output/mod.rs
+++ src/output/mod.rs
@@ -1,13 +1,16 @@
+pub mod serial;
 pub mod vga;
 
+/// Print to the VGA text buffer and over the first serial port.
 #[macro_export]
 macro_rules! print {
     ($($arg: tt)*) => {
         $crate::output::vga::_print(format_args!($($arg)*));
+        $crate::output::serial::_print(format_args!($($arg)*));
     };
 }
 
-/// Prints to the host through the serial interface, appending a newline.
+/// Print to the VGA text buffer and over the first serial port, appending a newline.
 #[macro_export]
 macro_rules! println {
     () => ($crate::print!("\n"));
