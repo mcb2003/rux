ef4053c9aa9bd16744867194cecee436f427d707
mcb2003 <mikeybuchan@hotmail.co.uk>
2021-10-15 13:57:30 +0100

    Format code

--- src/lib.rs
+++ src/lib.rs
@@ -11,8 +11,10 @@ mod output;
 use core::panic::PanicInfo;
 
 #[panic_handler]
+
 fn panic(info: &PanicInfo) -> ! {
     println!("Kernel {}", info);
+
     hlt_loop();
 }
 
--- src/output/mod.rs
+++ src/output/mod.rs
@@ -3,6 +3,7 @@ pub mod vga;
 
 /// Print to the VGA text buffer and over the first serial port.
 #[macro_export]
+
 macro_rules! print {
     ($($arg: tt)*) => {
         $crate::output::vga::_print(format_args!($($arg)*));
@@ -12,6 +13,7 @@ macro_rules! print {
 
 /// Print to the VGA text buffer and over the first serial port, appending a newline.
 #[macro_export]
+
 macro_rules! println {
     () => ($crate::print!("\n"));
     ($fmt: expr) => ($crate::print!(concat!($fmt, "\n")));
